733d326b7885972a3e70ecae4c5d9ef9e0976892
Abhigupta275 <abgupta275@gamil.com>
2023-10-21 19:56:22 +0530

    first code

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Signup } from '../features/Auth.js/components/Signup'
+
+function SignupPage() {
+  return (
+    <div>
+      <Signup></Signup>
+    </div>
+  )
+}
+
+export default SignupPage
